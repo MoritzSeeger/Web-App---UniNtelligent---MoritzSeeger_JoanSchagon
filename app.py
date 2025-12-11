b2d28f74e8263e1116144c99dfa90f0edb2bc8b7
Moritz Seeger <seegermoritz4@gmail.com>
2025-12-11 18:47:16 +0100

    11.12.25: web app work in progress.

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/dozenten')
+def dozenten_suche():
+    return render_template('dozenten_suche.html')
+
+@app.route('/dozenten/<int:id>')
+def dozenten_profil():
+    return render_template('dozenten_profil.html')
+
+@app.route('/kurse')
+def kurs_uebersicht():
+    return render_template('kursübersicht.html')
+
+@app.route('/kurse/<int:id>')
+def kurs_profil():
+    return render_template('kurs_profil.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
